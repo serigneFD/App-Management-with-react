6e52bc7ae708d063585edb7e6291e2c90005a66d
serigneFD <sfadiouf98@gmail.com>
2025-02-19 19:01:33 -0500

    Mon Application d'ajout de project complet et task pour les projet

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,132 @@
+import { useState } from "react";
+import ProjectSideBar from "./compoents/ProjectSideBar";
+import NewProject from "./compoents/NewProject";
+import NoProjectSelected from "./compoents/NoProjectSelected";
+import SelectedProject from "./compoents/SelectedProject";
+
 function App() {
+  
+  const [projectState, SetSelectedProject] = useState( {
+    projectSelectedID: undefined,
+    projects : [],
+    tasks: []
+   });
+
+   function handleAddTask(text) {
+    SetSelectedProject(prevState => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId : prevState.projectSelectedID,
+        id: taskId
+       };
+
+      return {
+        ...prevState,
+        tasks : [newTask, ...prevState.tasks]
+      }
+    })
+   }
+
+   function handleDeleteTask(id) {
+    SetSelectedProject((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => 
+          task.id !== id)
+      }
+    })
+   }
+  
+  function handlebuttonNewproject() {
+    SetSelectedProject(prevState => {
+      return {
+        ...prevState,
+        projectSelectedID: null,
+      }
+    })
+  }
+     // handle l'ajout du project 
+   function handleAddProject(projectData) {
+  
+    SetSelectedProject(prevState => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId
+       };
+
+      return {
+        ...prevState,
+        projectSelectedID: undefined,
+        projects : [...prevState.projects, newProject]
+      }
+    })
+   }
+
+   
+ // console.log(projectState);
+  
+ // to handle the button Cancel
+ function handleCancelAddProject () {
+  SetSelectedProject((prevState) => {
+    return{
+      ...prevState,
+      projectSelectedID:undefined
+    }
+  })
+ }
+
+ function handleSelectedProject(id) {
+  SetSelectedProject((prevState) => {
+    return {
+      ...prevState,
+      projectSelectedID: id,
+      projects: prevState.projects.filter((project) => 
+      project.id !== prevState.projectSelectedID)
+    }
+  })
+ }
+
+ // pour gerer le button delete de SelectedProject
+ function handleDeletetedProject () {
+  SetSelectedProject((prevState) => {
+    return{
+      ...prevState,
+      projects
+    }
+  })
+ }
+   
+ // trouver l'element du tableau selectionner
+   const selectedproject = projectState.projects.find(project => project.id === projectState.projectSelectedID);
+   let content = <SelectedProject 
+   project={selectedproject} 
+   onDelete={handleDeletetedProject}
+   onAddTask={handleAddTask}
+   onDeleteTask={handleDeleteTask}
+   tasks={projectState.tasks}
+   />;
+
+   if(projectState.projectSelectedID === null) {
+     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+   } 
+   else if(projectState.projectSelectedID === undefined) 
+   {
+    content = <NoProjectSelected onslectedButton={handlebuttonNewproject} />
+   }
+   
+  
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+     <ProjectSideBar 
+     onslectedButton={handlebuttonNewproject} 
+     projects={projectState.projects}
+     onSelect={handleSelectedProject}
+     />
+     {content}
+    </main>
   );
 }
 
---
+++ src/compoents/SelectedProject.jsx
@@ -0,0 +1,32 @@
+import Tasks from "./Tasks"
+
+export default function SelectedProject({project, 
+    onDelete,
+    onAddtask,
+    onDeleteTask,
+    tasks
+}) 
+{
+    const formatedDate = new Date(project.dueDate).toLocaleDateString('en-US',{
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric'
+    })
+    return (
+        <div className="w-[35rem] mt-16">
+            <header className="pb-4 mb-4 border-b-2 border-stone-300">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-3xl font-bold text-stone-600 mb-2">
+                        {project.title}
+                    </h1>
+                    <button onClick={onDelete} className="text-stone-600 hover:text-stone-950">
+                        Delete
+                    </button>
+                </div>
+                <p className="mb-4 text-stone-400">{project.dueDate}</p>
+                <p className="text-stone-600 whitespace-pre-line">{project.description}</p>
+            </header>
+            <Tasks onAdd={onAddtask} onDelete={onDeleteTask} tasks={tasks} />
+        </div>
+    )
+}
